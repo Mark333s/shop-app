3c741f279b72be07bfe352092d7c3fafa490f02b
Mark333s <markomelnychyn333@gmail.com>
2022-01-29 11:44:12 +0000

    create structure app

---
+++ shop/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import { ReactDOM } from 'react';
+
+function App() {
+  return (
+    <>
+    
+    
+    </>
+  );
+}
+
+export default App;
---
+++ shop/src/product-list/list-item.js
@@ -0,0 +1,17 @@
+import React from "react";
+import products from '../product/product'
+
+function List () {
+    return (
+        <>
+     
+        
+        
+        </>
+    )
+}
+
+
+
+
+export default List;
